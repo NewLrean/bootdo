3fe87437bfdc20ecebb9954ae66774e7b9082beb
master <2210373713@qq.com>
2019-07-18 15:53:58 +0800

    update

--- bootdo/src/test/java/com/bootdo/testDemo/TestDemo.java
+++ bootdo/src/test/java/com/bootdo/testDemo/TestDemo.java
@@ -1,21 +1,16 @@
 package com.bootdo.testDemo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RestController()
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TestDemo {
     @Autowired
     RedisTemplate redisTemplate;
 
-    @Test
+   // @Test
     public void test() {
         redisTemplate.opsForValue().set("a", "b");
         System.out.println(redisTemplate.opsForValue().get("a"));
